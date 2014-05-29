91dfa00ad0aeebd1f8be74931e6dac46f341c62a
Jan Vojt <jan@vojt.net>
2014-05-29 23:19:30 +0200

    First commit with empty project.

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: janvojt
+ *
+ * Created on May 29, 2014, 9:53 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "hello world!" << endl;
+    return 0;
+}
+
