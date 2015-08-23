c80b4a9a74685bf678326fc0c732b08c2bef1d3f
Jan Vojt <jan@vojt.net>
2015-08-23 18:46:34 +0200

    Adjusted parameters of convolutional network.

--- examples/mnist-conv.sh
+++ examples/mnist-conv.sh
@@ -6,6 +6,6 @@ basedir=`dirname $script`
 cd "$basedir/.."
 
 ulimit -t unlimited
-time ./bin/ffwdnet -b -i -c examples/mnist-conv.cfg \
+time ./bin/ffwdnet -l 0.001 -a-0.3,0.3 -b -i -c examples/mnist-conv.cfg \
  -t resources/mnist/t10k-images-idx3-ubyte:resources/mnist/t10k-labels-idx1-ubyte \
- -s resources/mnist/train-images-idx3-ubyte:resources/mnist/train-labels-idx1-ubyte
+ -s resources/mnist/train-images-idx3-ubyte:resources/mnist/train-labels-idx1-ubyte $@
