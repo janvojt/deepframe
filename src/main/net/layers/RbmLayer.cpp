d10a59d4ebd6dca31ad6656a3b3fd700a834e42c
Jan Vojt <jan@vojt.net>
2016-04-03 10:40:48 +0200

    Added proper support for persistent CD.

--- src/main/net/layers/RbmLayer.cpp
+++ src/main/net/layers/RbmLayer.cpp
@@ -136,6 +136,7 @@ void RbmLayer::pretrainGpu() {
     
     // Reset the parameters sampled from previous training
     if (!conf.isPersistent || !samplesInitialized) {
+        samplesInitialized = true;
         int memSize = inputSize * sizeof(data_t);
         checkCudaErrors(cudaMemcpy(sInputs, inputs, memSize, cudaMemcpyDeviceToDevice));
         sample_vh_gpu();
@@ -263,12 +264,12 @@ void RbmLayer::processConfString(string confString) {
     
     iss >> sep;
     
-//    if (!(iss >> boolalpha >> conf.isPersistent)) {
-//        LOG()->warn("Could not read whether to use CD or PCD for RBM layer from configuration. Using CD...");
-//        conf.isPersistent = false;
-//    }
-//    
-//    iss >> sep;
+    if (!(iss >> boolalpha >> conf.isPersistent)) {
+        LOG()->warn("Could not read whether to use CD or PCD for RBM layer from configuration. Using CD...");
+        conf.isPersistent = false;
+    }
+    
+    iss >> sep;
     
     if (!(iss >> conf.gibbsSteps)) {
         LOG()->error("Could not read number of Gibbs steps for RBM layer, using 1.");
