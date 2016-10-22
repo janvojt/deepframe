49841286be3712aefe250b844a7ca707a0e0501e
Jan Vojt <jan@vojt.net>
2016-10-22 22:14:02 +0200

    Adjusted network configuration for kiwi dataset.

--- examples/kiwi-dbn.sh
+++ examples/kiwi-dbn.sh
@@ -6,6 +6,6 @@ basedir=`dirname $script`
 cd "$basedir/.."
 
 ulimit -t unlimited
-time ./bin/deepframe -n1 -m100 -e0.01 -l0.1 -a0.01 -g -c examples/kiwi-dbn.cfg -p \
- -t ~/workspace/kiwi/big.dat \
- -s ~/workspace/kiwi/small.dat $@
+time ./bin/deepframe -n1 -m2 -e0.001 -g -c examples/kiwi-dbn.cfg \
+ -t ../kiwi/small.dat \
+ -s ../kiwi/big.dat $@
